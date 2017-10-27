32cd737c6372d7f30578644de9136832f2ab8e3e
Jibril <j.d.ivan69@gmail.com>
2017-10-26 23:16:46 -0500

    Finished implementing interfaces of Events and Electrodomestics fragments OnItemSelected.

--- A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/MainActivity.java
+++ A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/MainActivity.java
@@ -2,12 +2,42 @@ package itesm.mx.a01191342_examenvinculacion_ahorroenergia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnEventSelectedListener, OnElectroSelectedListener{
+
+    public static String DEBUG_TAG = "TAG_MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onEventSelected(int position) {
+        switch (position) {
+            case 0:
+                Log.d(DEBUG_TAG, "Position Event: " + position);
+                break;
+            case 1:
+                Log.d(DEBUG_TAG, "Position Event: " + position);
+                break;
+        }
+    }
+
+
+    @Override
+    public void onElectroSelected(int position) {
+        switch (position) {
+            case 0:
+                Log.d(DEBUG_TAG, "Position Electrodomestic: " + position);
+                break;
+            case 1:
+                Log.d(DEBUG_TAG, "Position Electrodomestic: " + position);
+                break;
+        }
     }
 }
