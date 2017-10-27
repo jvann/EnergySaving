8bb2345499ee98fc49bad837336d3ce144f7b5a8
Jibril <j.d.ivan69@gmail.com>
2017-10-27 11:09:36 -0500

    .

--- savedenergy/app/src/main/java/itesm/mx/savedenergy/OnElectroSelectedListener.java
+++
@@ -1,9 +0,0 @@
-package itesm.mx.savedenergy;
-
-/**
- * Created by Jibril on 10/26/17.
- */
-
-public interface OnElectroSelectedListener {
-    public void onElectroSelected(int position);
-}
--- savedenergy/app/src/main/java/itesm/mx/savedenergy/OnEventSelectedListener.java
+++
@@ -1,9 +0,0 @@
-package itesm.mx.savedenergy;
-
-/**
- * Created by Jibril on 10/26/17.
- */
-
-public interface OnEventSelectedListener {
-    public void onEventSelected(int position);
-}
