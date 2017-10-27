8a889ea8b7b8821721ed13b629a08e4eb20cce2f
Jibril <j.d.ivan69@gmail.com>
2017-10-26 21:58:43 -0500

    Fragments created. Events and Electrodomestics.

---
+++ A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/OnEventSelectedListener.java
@@ -0,0 +1,9 @@
+package itesm.mx.a01191342_examenvinculacion_ahorroenergia;
+
+/**
+ * Created by Jibril on 10/26/17.
+ */
+
+public interface OnEventSelectedListener {
+    public void onEventSelected(int position);
+}
