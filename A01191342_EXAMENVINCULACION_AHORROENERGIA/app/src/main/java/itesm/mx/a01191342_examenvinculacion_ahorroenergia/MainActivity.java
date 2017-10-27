428ad5df5fcc78c2b17b0b16798f9daa9f8e4643
Jibril <j.d.ivan69@gmail.com>
2017-10-26 20:26:43 -0500

    Init commit

---
+++ A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/MainActivity.java
@@ -0,0 +1,13 @@
+package itesm.mx.a01191342_examenvinculacion_ahorroenergia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
