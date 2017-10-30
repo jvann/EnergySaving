96f9baa65af766bd6174cefc5f3a9c501df67282
Jibril <j.d.ivan69@gmail.com>
2017-10-30 16:26:43 -0600

    Checkpoint. Starting SQL.

--- A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/MainActivity.java
+++ A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/MainActivity.java
@@ -44,8 +44,7 @@ public class MainActivity extends AppCompatActivity implements OnItemClickedList
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+        // Create the adapter that will return a fragment for each tab.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
