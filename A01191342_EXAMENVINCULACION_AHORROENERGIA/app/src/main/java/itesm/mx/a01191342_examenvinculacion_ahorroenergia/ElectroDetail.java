f92ceab265ee623183af5c22718097a6a1dba645
Jibril <j.d.ivan69@gmail.com>
2017-11-03 13:43:38 -0600

    Gestures reactiong well. Applying fling method.

--- A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/ElectroDetail.java
+++ A01191342_EXAMENVINCULACION_AHORROENERGIA/app/src/main/java/itesm/mx/a01191342_examenvinculacion_ahorroenergia/ElectroDetail.java
@@ -1,9 +1,11 @@
 package itesm.mx.a01191342_examenvinculacion_ahorroenergia;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,12 +20,21 @@ public class ElectroDetail extends AppCompatActivity implements View.OnTouchList
     private TextView tvNameElectro;
     private TextView tvKwh;
 
+    private GestureDetectorCompat mDetector;
+    private MyGestureListener myGestureListener;
+
     private static String DEBUG_TAG = "ELECTRO_DETAIL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electro_detalle);
+
+        View view = findViewById(R.id.layout_detalle_electro);
+        view.setOnTouchListener(this);
+
+        myGestureListener = new MyGestureListener();
+        mDetector = new GestureDetectorCompat(this, myGestureListener);
 
         ivPicture = (ImageView) findViewById(R.id.image_electro_detail);
         tvNameElectro = (TextView) findViewById(R.id.text_detail_electro);
@@ -36,6 +47,8 @@ public class ElectroDetail extends AppCompatActivity implements View.OnTouchList
             setViewDetail(data.getInt("position"));
         }
 
+
+
     }
 
     private void setViewDetail(int position) {
@@ -46,27 +59,78 @@ public class ElectroDetail extends AppCompatActivity implements View.OnTouchList
 
         @Override
         public boolean onTouch(View view, MotionEvent event) {
-            int action = event.getAction();
-
-            switch(action) {
-                case (MotionEvent.ACTION_DOWN) :
-                    Log.d(DEBUG_TAG,"Action was DOWN");
-                    return true;
-                case (MotionEvent.ACTION_MOVE) :
-                    Log.d(DEBUG_TAG,"Action was MOVE");
-                    return true;
-                case (MotionEvent.ACTION_UP) :
-                    Log.d(DEBUG_TAG,"Action was UP");
-                    return true;
-                case (MotionEvent.ACTION_CANCEL) :
-                    Log.d(DEBUG_TAG,"Action was CANCEL");
-                    return true;
-                case (MotionEvent.ACTION_OUTSIDE) :
-                    Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
-                            "of current screen element");
-                    return true;
-                default :
-                    return super.onTouchEvent(event);
-            }
+//            int action = event.getAction();
+//
+//            switch(action) {
+//                case (MotionEvent.ACTION_DOWN) :
+//                    Log.d(DEBUG_TAG,"Action was DOWN");
+//                    return true;
+//                case (MotionEvent.ACTION_MOVE) :
+//                    Log.d(DEBUG_TAG,"Action was MOVE");
+//                    return true;
+//                case (MotionEvent.ACTION_UP) :
+//                    Log.d(DEBUG_TAG,"Action was UP");
+//                    return true;
+//                case (MotionEvent.ACTION_CANCEL) :
+//                    Log.d(DEBUG_TAG,"Action was CANCEL");
+//                    return true;
+//                case (MotionEvent.ACTION_OUTSIDE) :
+//                    Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
+//                            "of current screen element");
+//                    return true;
+//                default :
+//                    return super.onTouchEvent(event);
+//            }
+            mDetector.onTouchEvent(event);
+            return true;
         }
+
+    public class MyGestureListener implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener  {
+
+        public String DEBUG_TAG = "GESTURE_LISTENER";
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onShowPress");
+        }
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onLongPress");
+        }
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onSingleTapUp");
+            return false;
+        }
+        @Override
+        public boolean onScroll(MotionEvent e, MotionEvent e2, float distanceX, float distanceY) {
+            Log.d(DEBUG_TAG, "onScroll");
+            return false;
+        }
+        @Override
+        public boolean onFling(MotionEvent e, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(DEBUG_TAG, "onFling");
+            return false;
+        }
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onDown");
+            return false;
+        }
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onSingleTapConfirmed");
+            return false;
+        }
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onDoubleTap");
+            return false;
+        }
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onDoubleTapEvent");
+            return false;
+        }
+    }
 }
